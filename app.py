30f623281f1042afcc4a835df027a0d18f1d545b
Aurimas Racas <mail@aurimas.eu>
2024-05-26 23:15:06 -0400

    Various tweaks

--- app.py
+++ app.py
@@ -12,27 +12,24 @@ st.markdown("# Long-term error rate calculator")
 
 with st.expander("Intro"):
     c1, c2 = st.columns(2, gap="large")
+    st.text("-- Aurimas Racas, 2024")
 
 c1.markdown("""
 ## What is this about?
 
-This Streamlit app helps estimating long-term experiment rates for simple A/B tests with a binary outcome metric:
+This Streamlit app accompanies [my blog post](https://aurimas.eu/blog/2024/05/estimating-long-term-detection-win-and-error-rates-in-a-b-testing/) on estimating long-term (i.e. over a series of different experiments) detection, win, and error rates.
+
+Specifically, for simple A/B tests with a binary outcome metric, it helps estimating:
 
  * **Detection rate:** % of experiments in which statistically significant result will be detected;
  * **Win rate:** % of experiments where a positive statistically significant effect will be detected;
  * **Sign error rate:** % of experiments that we will detect a significant effect, but it will be of opposite sign than the true underlying effect;
  * **Exaggeration rate:** Average difference between the observed effect size that was declared as statistically significant and the true underlying effect size (an estimate of winner's curse phenomenon)
 
-Getting to such estimates requires two inputs:
-
- * Sample size and baseline metric
- * Assumptions about effect size distribution
-
-The calculator also estimates the "classical" frequentist statistical power (detection rate, but for a single experiment if repeated many times) as well as two other quantities proposed by Gelman and Carlin:
+The calculator also estimates the frequentist statistical power (detection rate, but for a single experiment if repeated many times) as well as two other quantities proposed by Gelman and Carlin:
 
  * Type S error rate (sign error rate, but for a single experiment)
  * Type M rate (exaggeration rate, expressed as a ratio between observed and true effect size)
-
 
 """)
 
@@ -42,21 +39,6 @@ c2.markdown("""
 
  * Get estimates of long-term win and error rates for your experimentation program! Frequentist statistics don't provide these alone.
  * Explore the relationship between sample size and expected detection rates. You'll notice that MDE (and thus statistical power) doesn't affect them! That's because MDE is just an assumption, and the only thing that actually matters is sample size itself and the assumption on effect sizes. It may convince you to rethink the MDE used in your power calculations.
- * Rethink how much to focus on Type I error rates, given that null hypothesis is unlikely to be true in most settings, and thus Type S error rate may be much more useful.
-
- #
- #
- #
- #
- #
- #
- #
- #
- 
-
- More details @ my blog post [Statistical guarantees and long-term error rates in A/B testing](https://aurimas.eu).
-
- -- Aurimas Racas, 2024
 """)
 
 
@@ -69,7 +51,7 @@ with power.expander("Basic inputs", expanded=True):
         min_value=1000,
         max_value=50_000,
         step=1000,
-        value=35_000,
+        value=10_000,
     )
 
     baseline_rate = st.slider(
@@ -77,7 +59,7 @@ with power.expander("Basic inputs", expanded=True):
         min_value=0.01,
         max_value=0.99,
         step=0.01,
-        value=0.5,
+        value=0.17,
     )
 
     MDE = st.slider(
@@ -85,7 +67,7 @@ with power.expander("Basic inputs", expanded=True):
         min_value=0.001,
         max_value=0.1,
         step=0.001,
-        value=0.01,
+        value=0.015,
         format="%f",
     )
 
@@ -93,47 +75,49 @@ with power.expander("Basic inputs", expanded=True):
         delta=MDE, sd=np.sqrt(baseline_rate * (1 - baseline_rate)), n=sample_size
     )
 
-    cols = st.columns(3)
-
-    cols[0].metric("Statistical power", value=f"{rates['power']:.0%}")
-    cols[1].metric("Type S error rate (at MDE)", value=f"{rates['typeS']:.2%}")
-    cols[2].metric("Type M ratio (at MDE)", value=f"{rates['typeM']:.2f}")
+    st.metric("Statistical power (at MDE)", value=f"{rates['power']:.0%}")
+    st.metric("Type S error rate (at MDE)", value=f"{rates['typeS']:.5%}")
+    st.metric("Type M ratio (at MDE)", value=f"{rates['typeM']:.2f}")
 
 with prior.expander(
-    "Assumed distribution of effect sizes across experiments", expanded=True
+    "Assumed distribution of effect sizes across experiments (modelled as Gamma distribution)",
+    expanded=True,
 ):
-    cols = st.columns((1, 2))
+    cols = st.columns(3)
 
     loc = cols[0].slider(
-        "Location parameter (changes center point of distribution)",
-        value=-0.015,
+        "Location parameter, $\\mu$",
+        value=-0.005,
         min_value=-0.05,
-        max_value=0.05,
+        max_value=0.0,
         step=0.001,
         format="%f",
+        help="changes center point of distribution",
     )
-    scale = cols[0].slider(
-        "Scale parameter (changes how varied effect sizes are)",
-        value=0.01,
+    scale = cols[1].slider(
+        "Scale parameter, $\\beta$",
+        value=0.005,
         min_value=0.001,
-        max_value=0.03,
+        max_value=0.01,
         step=0.001,
         format="%f",
+        help="Changes how varied effect sizes are",
     )
 
-    a = cols[0].slider(
-        "$\\alpha$ parameter (changes how 'protected' the downside is)",
+    a = cols[2].slider(
+        "Shape parameter, $\\alpha$",
         value=2.0,
         min_value=1.0,
-        max_value=7.0,
+        max_value=5.0,
         step=0.1,
         format="%f",
+        help="Changes how skewed the effect sizes are",
     )
 
     rv = sts.gamma(loc=loc, scale=scale, a=a)
 
     x = np.linspace(rv.ppf(0.001), rv.ppf(0.999), 1000)
-    fig, ax = plt.subplots(1, 1, figsize=(4, 2))
+    fig, ax = plt.subplots(1, 1, figsize=(5, 2))
     density = rv.pdf(x)
     ax.plot(x, density, lw=1, alpha=0.6, label="gamma pdf", color="#1b9e77")
     plt.fill_between(x, rv.pdf(x), lw=1, alpha=0.6, label="gamma pdf", color="#1b9e77")
@@ -171,9 +155,9 @@ with prior.expander(
         fontsize=5,
         color="#d95f02",
     )
-    ax.set_xlim(xmin=-0.05, xmax=0.2)
+    ax.set_xlim(xmin=-0.05, xmax=0.1)
 
-    cols[1].pyplot(fig, use_container_width=True)
+    st.pyplot(fig, use_container_width=True)
 
     summary_stats = pd.DataFrame(
         [
@@ -184,14 +168,30 @@ with prior.expander(
         ]
     )
 
-    cols[1].write(f"Average effect size: `{rv.mean():.3f}`")
-    cols[1].write(
-        f"Worst 10% of experiments will have effect size below: `{rv.ppf(0.1):.3f}`"
+    st.markdown("##### True (unobservable) effects")
+
+    st.dataframe(
+        pd.DataFrame(
+            {
+                "Metric": [
+                    "Average effect size",
+                    "% experiments with positive impact",
+                    "Q10 effect size",
+                    "Q50 effect size",
+                    "Q90 effect size",
+                ],
+                "Value": [
+                    f"{rv.mean():.1%}",
+                    f"{ (1- rv.cdf(0)):.0%}",
+                    f"{rv.ppf(0.1):.1%}",
+                    f"{rv.ppf(0.5):.1%}",
+                    f"{rv.ppf(0.9):.1%}",
+                ],
+            }
+        ),
+        hide_index=True,
+        use_container_width=True,
     )
-    cols[1].write(
-        f"Top 10% of experiments will have effect size above: `{rv.ppf(0.9):.3f}`"
-    )
-    cols[1].write(f"Share of experiments that have negative impact: `{rv.cdf(0):.0%}`")
 
     with lt_rates.expander(label="Long term rates", expanded=True):
         st.metric(
